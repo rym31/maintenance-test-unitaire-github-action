6a1f17a92a52abda8ef1a3b9328aa8402fb414f6
rym31 <rym4085@outlook.com>
2026-09-21 14:35:40 -0400

    Ajout des mocks

--- src/test/java/ca/cegepmv/atelier/CalculatriceAvecHistoriqueTest.java
+++ src/test/java/ca/cegepmv/atelier/CalculatriceAvecHistoriqueTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
@@ -54,12 +55,15 @@ class CalculatriceAvecHistoriqueTest {
     void additionnerEnregistreUneEntreeDansLeJournal() {
         // TODO: Act
         // Appelez calculatrice.additionner(2, 3)
+        calculatrice.additionner(2, 3);
 
         // TODO: Assert
         // Utilisez verify(journalMock).enregistrer(...) pour vérifier qu'un message a bien
         // été enregistré. Astuce : vous pouvez vérifier le message exact attendu :
         // verify(journalMock).enregistrer("2 + 3 = 5");
-        fail("Test à compléter");
+        verify(journalMock).enregistrer("2 + 3 = 5");
+
+        // fail("Test à compléter");
     }
 
     @Test
@@ -69,6 +73,10 @@ class CalculatriceAvecHistoriqueTest {
         // ET utilisez verify(journalMock, never()).enregistrer(anyString()) pour vérifier
         // qu'aucun message n'a été enregistré dans ce cas (puisque l'exception est lancée
         // AVANT l'appel à journal.enregistrer(...)).
-        fail("Test à compléter");
+        assertThrows(ArithmeticException.class,() -> calculatrice.diviser(10, 0));
+        verify(journalMock, never()).enregistrer(anyString());
+
+
+        // fail("Test à compléter");
     }
 }
